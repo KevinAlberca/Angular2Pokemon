2c25b00406a90e27fc69e7aea8de882db51de8ac
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-12-14 11:11:17 +0100

    [FEAT] Make an «/pokemon» routes

---
+++ src/app/Pokemon/pokemon_list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './pokemon_list.component.html',
+  styleUrls: ['./pokemon_list.component.css']
+})
+export class PokemonListComponent {
+  title = 'Which pokemon ?';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,28 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 
 // Personal import Here
+import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from '@angular/material';
+import { PokemonListComponent } from './Pokemon/pokemon_list.component';
+
+const appRoutes: Routes = [
+    { path: 'pokemon', component: PokemonListComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-// Personal Import
-    MaterialModule.forRoot()
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent, 
+        PokemonListComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        // Personal Import
+        RouterModule.forRoot(appRoutes),
+        MaterialModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
