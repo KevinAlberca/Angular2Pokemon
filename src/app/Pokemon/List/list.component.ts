d2d0b2f13ea9c6e990f125b90556716f62f5d1c3
Kevin Alberca <kevin.alberca@supinternet.fr>
2017-01-04 21:28:52 +0100

    Add pokemon list in src/app/Pokemon/List

---
+++ src/app/Pokemon/List/list.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Http } from "@angular/http";
+import { Pokemon } from '../Pokemon';
+import 'rxjs/Rx';
+
+@Component({
+    selector: 'pokemon-list',
+    templateUrl: './pokemon_list.component.html',
+    styleUrls: ['./pokemon_list.component.css']
+})
+
+export class PokemonListComponent {
+    title = 'Which pokemon ?';
+    private apiUrl: string = "https://pokeapi.co/api/v2/";
+
+    pokemons: Array<Pokemon>;
+
+    constructor(private http: Http) {
+        /*
+         * @strRequest: string - the url needed to fetch data on pokeapi
+         * @response.json().results: JSON Object - Return the returned data["results"] to JSON
+         */
+        let strRequest: string =  this.apiUrl + 'pokemon';
+
+        console.log('strRequest : ', strRequest)
+        this.http.get(strRequest)
+            .map( (response) => {
+                return response.json().results;
+            })
+            .map((pokemons: Array<any>) => {
+                let result: Array<Pokemon> = []; // Define an empty array. It will be append with the fetched data
+                if (pokemons) {
+                    pokemons.forEach( (poke) => { // For each fetched pokemons, create new pokemon object
+                        result.push(new Pokemon(poke.id, poke.name, poke.url)); // Push
+                    });
+                    return result;
+                }
+            })
+            .subscribe( (res) => {
+                this.pokemons = res
+            });
+    }
+}
