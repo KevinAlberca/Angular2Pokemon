564d5978ca448940e915f203fa97e68e7bdbbe7a
Kevin Alberca <kevin.alberca@supinternet.fr>
2017-01-07 22:19:26 +0100

    [WIP] Fetch data for pokemon details

--- src/app/Pokemon/Details/details.component.ts
+++ src/app/Pokemon/Details/details.component.ts
@@ -13,6 +13,7 @@ export class PokemonDetailsComponent {
     private sub:any;
     pokemon_watched: string;
     private apiUrl: string = "https://pokeapi.co/api/v2/";
+    pokemon: Pokemon;
 
     constructor(private route: ActivatedRoute, private http: Http) {}
 
@@ -23,9 +24,47 @@ export class PokemonDetailsComponent {
         this.sub = this.route.params.subscribe(params => {
             this.pokemon_watched = params['name'];
         });
+        this.getPokemonInformations(this.pokemon_watched);
+
     }
 
     ngOnDestroy() {
         this.sub.unsubscribe();
+    }
+
+    getPokemonInformations(pokemon_name: string) {
+        /*
+         * @strRequest: string - the url needed to fetch data on pokeapi, concatenate with pokemon passed in params
+         * @response.json().results: JSON Object - Return the returned data["results"] to JSON
+         */
+        let strRequest: string =  this.apiUrl + 'pokemon/' + pokemon_name;
+        console.log('strRequest : ', strRequest)
+        this.http.get(strRequest)
+            .map( (response) => {
+                // console.log('Response : ', response.json());
+                return response.json();
+            })
+            .map((pokemon: Array<any>) => {
+                console.log('Pokemon Response : ');
+                console.log(pokemon);
+                console.log('Types : ', pokemon['types']);
+
+                // let types = pokemon['types'];
+                // for (var i = 0; i < types.length; i++) {
+                //     console.log(types[i].type.name);
+                // }
+
+                let poke_result: Pokemon = new Pokemon(
+                                   pokemon['id'], // id
+                                   pokemon['name'], // name
+                                   pokemon['abilities'], // abilities
+                                   pokemon['types'], // types
+                                   pokemon['sprites']['front_default'] // imageUrl
+                                );
+                return poke_result;
+            })
+            .subscribe( (res) => {
+                this.pokemon = res
+            });
     }
 }
--- src/app/Pokemon/Pokemon.ts
+++ src/app/Pokemon/Pokemon.ts
@@ -2,15 +2,18 @@ export class Pokemon {
 	id: number;
 	name: string;
 	abilities: Array<any>;
+	types: Array<any>;
 	imageUrl: string;
 
 	constructor(id,
 	            name,
 	            abilities,
+	            types,
 	            imageUrl) {
 		this.id = id;
 		this.name = name;
 		this.abilities = abilities;
+		this.types = types;
 		this.imageUrl = imageUrl;
 
 	}
